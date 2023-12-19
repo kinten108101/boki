bd2bc7a571c5cbba0e390dab31b6829465b67990
kinten108101 <kinten108101@protonmail.com>
2023-12-19 14:42:47 +0700

    quit action

--- src/application.js
+++ src/application.js
@@ -1,3 +1,4 @@
+import Gio from 'gi://Gio';
 import Adw from 'gi://Adw';
 
 import { Window } from './window.js';
@@ -6,6 +7,14 @@ export function Application() {
 	const application = new Adw.Application({
 		application_id: 'com.github.kinten108101.Boki',
 	});
+
+	const quit = new Gio.SimpleAction({
+		name: 'quit',
+	});
+	quit.connect('activate', () => {
+		application.quit();
+	});
+	application.add_action(quit);
 
   	application.connect('activate', () => {
     	const mainWindow = Window(application);
