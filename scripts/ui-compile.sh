fd40d856114cae6b5fa3a89803b0971d67512961
kinten108101 <kinten108101@protonmail.com>
2023-12-26 17:53:14 +0700

    New ui compile script

--- scripts/ui-compile.sh
+++ scripts/ui-compile.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env sh
-FILES=
-IFS=','
-for file in $@; do
-	FILES="${FILES} data/ui/src/${file}"
-done
-unset IFS
-blueprint-compiler batch-compile data/ui data/ui/src ${FILES}
+FILENAME=$1
+blueprint-compiler compile --output "data/ui/${FILENAME}.ui" "data/ui/src/${FILENAME}.blp"
