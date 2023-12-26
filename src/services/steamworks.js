8358a144914fb905a7b5a5d10c53fed9bea160cc
kinten108101 <kinten108101@protonmail.com>
2023-12-27 04:11:53 +0700

    Prettified steamworks.js

--- src/services/steamworks.js
+++ src/services/steamworks.js
@@ -17,10 +17,10 @@ const URL_GPFD = GLib.Uri.parse('https://api.steampowered.com/ISteamRemoteStorag
 const extract_workshop_ttem_id = (url) => {
     const idxParam = url.indexOf('?id=', 0);
     if (idxParam === undefined) {
-      throw new GLib.Error(
-        db_service_error_quark(),
-        DbServiceErrorEnum.IdNotFound,
-        `Could not extract id parameter from url \"${url}\"`);
+      	throw new GLib.Error(
+        	db_service_error_quark(),
+        	DbServiceErrorEnum.IdNotFound,
+        	`Could not extract id parameter from url \"${url}\"`);
     }
 
     let idxParamEnd = url.indexOf('&', idxParam);
@@ -28,10 +28,10 @@ const extract_workshop_ttem_id = (url) => {
 
     const fileId = url.substring(idxParam + 4, idxParamEnd);
     if (!is_number_string(fileId)) {
-      throw new GLib.Error(
-        db_service_error_quark(),
-        DbServiceErrorEnum.IdNotDecimal,
-        `Supposed id parameter in url \"${url}\" is not in decimal format`);
+      	throw new GLib.Error(
+        	db_service_error_quark(),
+        	DbServiceErrorEnum.IdNotDecimal,
+        	`Supposed id parameter in url \"${url}\" is not in decimal format`);
     }
     return fileId;
 }
@@ -43,17 +43,17 @@ const extract_workshop_ttem_id = (url) => {
 const extract_author_name = playerDetails => {
 	const personaname = playerDetails['personaname'];
     if (!(typeof personaname !== 'string' || personaname === ''))
-      return personaname;
+      	return personaname;
 
     const profileurl = playerDetails['profileurl'];
     const idIdx = profileurl?.indexOf('/id/') || -1;
     const vanityId = profileurl?.substring(idIdx + 4, profileurl?.length - 1) || '';
     if (!(idIdx === -1 || is_number_string(vanityId))) // not vanityid found
-      return vanityId;
+      	return vanityId;
 
     const realname = playerDetails['realname'];
     if (!(typeof realname !== 'string' || realname === ''))
-      return realname;
+      	return realname;
 
     throw new GLib.Error(
     	db_service_error_quark(),
@@ -79,15 +79,15 @@ export const Steamworks = (session, cancellable_ref) => {
 	    });
 	    const requestBody = new GLib.Bytes(DefaultEncoder.encode(`itemcount=1&publishedfileids%5B0%5D=${steam_id}`));
 	    msg.set_request_body_from_bytes(
-	      'application/x-www-form-urlencoded',
-	      requestBody,
+	      	'application/x-www-form-urlencoded',
+	      	requestBody,
 	    );
 	    const gbytes = await session.send_and_read_async(msg, GLib.PRIORITY_DEFAULT, cancellable_ref?.current);
 	    if (msg.status_code !== 200) {
-	      throw new GLib.Error(
-	        db_service_error_quark(),
-	        DbServiceErrorEnum.RequestNotSuccessful,
-	        `Request was not successful. Received a response status code of \"${msg.status_code}\"`);
+	      	throw new GLib.Error(
+	        	db_service_error_quark(),
+	        	DbServiceErrorEnum.RequestNotSuccessful,
+	        	`Request was not successful. Received a response status code of \"${msg.status_code}\"`);
 	    }
 	    const bytes = gbytes.get_data();
 	    if (bytes === null) throw new Error;
@@ -103,17 +103,17 @@ export const Steamworks = (session, cancellable_ref) => {
 		const uri = GLib.Uri.parse(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${WEBAPI}&steamids=${user_id}`, GLib.UriFlags.NONE);
 
 	    const msg = new Soup.Message({
-	      method: 'GET',
-	      uri,
+	      	method: 'GET',
+	      	uri,
 	    });
 
 	    const gbytes = await session.send_and_read_async(msg, GLib.PRIORITY_DEFAULT, null);
 	    const bytes = gbytes.get_data();
 	    if (msg.status_code !== 200) {
-	      throw new GLib.Error(
-	        db_service_error_quark(),
-	        DbServiceErrorEnum.RequestNotSuccessful,
-	        `Request was not successful. Received a response status code of \"${msg.status_code}\"`);
+	      	throw new GLib.Error(
+	        	db_service_error_quark(),
+	        	DbServiceErrorEnum.RequestNotSuccessful,
+	        	`Request was not successful. Received a response status code of \"${msg.status_code}\"`);
 	    }
 	    if (bytes === null) throw new Error;
 	    const response = read_json_bytes(bytes);
