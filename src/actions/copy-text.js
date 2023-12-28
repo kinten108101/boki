75a108a5db236ff658800cdf9edea67fb0ea818e
kinten108101 <kinten108101@protonmail.com>
2023-12-29 00:16:29 +0700

    copy-text to use universal builder action target binding parser

--- src/actions/copy-text.js
+++ src/actions/copy-text.js
@@ -7,6 +7,7 @@ import Adw from 'gi://Adw';
 import { gettext as _ } from "gettext";
 
 import { TOAST_TIMEOUT_SHORT } from '../utils/gtk.js';
+import { get_value_from_formatting_string } from '../utils/builder.js';
 
 /**
  * @param {Gtk.Widget} provider
@@ -18,34 +19,17 @@ export function useCopyText(provider, builder, toaster) {
 
 	const action = new Gio.SimpleAction({
 		name: 'copy-text',
-		parameter_type: GLib.VariantType.new('s'),
+		parameter_type: GLib.VariantType.new_tuple([GLib.VariantType.new('s')]),
 	});
 	action.connect('activate', (_action, parameter) => {
 		if (!parameter) throw new Error;
-		const [value] = parameter.get_string();
-		if (value === null) throw new Error;
-		let str = '';
-		if (value.length > 9 && value.substring(0, 8) === 'builder(' && value.lastIndexOf(')') == (value.length - 1)) {
-			if (!builder) throw new Error;
-			const path = value.substring(8, value.length - 1);
-			const segments = path.split('.');
-			if (segments.length < 2) throw new Error;
-			const obj_name = (/** @type {string} */(segments[0]));
-			console.debug('obj_name', obj_name);
-			const obj = builder.get_object(obj_name);
-			if (!obj) throw new Error;
-			/** @type {any} */
-			let val = obj;
-			for (let i = 1; i < segments.length; i++) {
-				const key = (/** @type {string} */(segments[i]));
-				console.debug('key', i, key);
-				val = val[key];
-			}
-			if (typeof val !== 'string') throw new Error;
-			str = val;
-		} else {
-			str = value;
-		}
+		const values = parameter.recursiveUnpack();
+		if (!Array.isArray(values))
+			throw new Error;
+
+		const str = /** @type {string | null} */(get_value_from_formatting_string(values[0], builder));
+		if (str === null) throw new Error;
+
       	const display = Gdk.Display.get_default();
       	if (display === null) return;
       	const val = new GObject.Value();
