8e67c4f5a7bd188aae691d3f9abcbaed7f7c1831
kinten108101 <kinten108101@protonmail.com>
2024-01-09 19:27:31 +0700

    Degrouped app setup procedure

--- src/application.js
+++ src/application.js
@@ -19,122 +19,119 @@ const get_xdg_download_dir = async () => {
     	throw new Error(stderr || '');
 };
 
-export function Application() {
-	GLib.log_set_debug_enabled(globalThis.is_devel);
+GLib.log_set_debug_enabled(globalThis.is_devel);
 
-	GLib.set_application_name('Boki');
+GLib.set_application_name('Boki');
 
-	const application = new Adw.Application({
-		application_id: 'com.github.kinten108101.Boki',
-	});
+export const application = new Adw.Application({
+	application_id: 'com.github.kinten108101.Boki',
+});
 
-	const settings = new Gio.Settings({
-		schema_id: 'com.github.kinten108101.Boki',
-	});
+const settings = new Gio.Settings({
+	schema_id: 'com.github.kinten108101.Boki',
+});
 
-	if (settings.get_boolean('first-launch')) {
-		(async () => {
-			const download_dir = await get_xdg_download_dir();
-			settings.set_string('download-directory', retract_path(download_dir));
+if (settings.get_boolean('first-launch')) {
+	(async () => {
+		const download_dir = await get_xdg_download_dir();
+		settings.set_string('download-directory', retract_path(download_dir));
 
-			settings.set_boolean('first-launch', false);
-		})().catch(logError);
-	}
+		settings.set_boolean('first-launch', false);
+	})().catch(logError);
+}
 
-	const quit = new Gio.SimpleAction({
-		name: 'quit',
-	});
-	quit.connect('activate', () => {
-		application.quit();
-	});
-	application.add_action(quit);
-	application.set_accels_for_action('app.quit', ['<Primary>q']);
+const quit = new Gio.SimpleAction({
+	name: 'quit',
+});
+quit.connect('activate', () => {
+	application.quit();
+});
+application.add_action(quit);
+application.set_accels_for_action('app.quit', ['<Primary>q']);
 
-	const new_window = new Gio.SimpleAction({
-		name: 'new-window',
-	});
-	new_window.connect('activate', () => {
-		const mainWindow = Window(application, settings);
-    	mainWindow.present();
-	});
-	application.add_action(new_window);
-	application.set_accels_for_action('app.new-window', ['<Primary>n']);
+const new_window = new Gio.SimpleAction({
+	name: 'new-window',
+});
+new_window.connect('activate', () => {
+	const mainWindow = Window(application, settings);
+	mainWindow.present();
+});
+application.add_action(new_window);
+application.set_accels_for_action('app.new-window', ['<Primary>n']);
 
+const show_pref = new Gio.SimpleAction({
+	name: 'show-preferences'
+});
+show_pref.connect('activate', () => {
+	const builder = new Gtk.Builder();
+	builder.add_from_resource('/com/github/kinten108101/Boki/ui/preferences.ui');
 
-	const show_pref = new Gio.SimpleAction({
-		name: 'show-preferences'
-	});
-	show_pref.connect('activate', () => {
-		const builder = new Gtk.Builder();
-		builder.add_from_resource('/com/github/kinten108101/Boki/ui/preferences.ui');
+	const window = (/** @type {Adw.PreferencesWindow | null} */(builder.get_object('preferences')));
+	if (!window)
+		throw new Error;
 
-
-		const window = (/** @type {Adw.PreferencesWindow | null} */(builder.get_object('preferences')));
-		if (!window)
+	if (globalThis.is_devel) {
+		const devel_page = (/** @type {Adw.PreferencesPage | null} */(builder.get_object('devel_page')));
+		if (!devel_page)
 			throw new Error;
 
-		if (globalThis.is_devel) {
-			const devel_page = (/** @type {Adw.PreferencesPage | null} */(builder.get_object('devel_page')));
-			if (!devel_page)
-				throw new Error;
+		window.add(devel_page);
+	}
 
-			window.add(devel_page);
+	const { output: _files, output_signals: file_signals, enable_action, disable_action } = useFile(window, builder, window);
+
+	const default_directory_row = /** @type {Adw.ActionRow | null} */ (builder.get_object('default_directory_row'));
+	if (!default_directory_row) throw new Error;
+
+	const on_download_directory_changed = () => {
+		const value = settings.get_string('download-directory');
+		if (value === '') {
+			default_directory_row.set_subtitle(_('Select Location'));
+			disable_action('file.set', 'download-directory');
+		} else {
+			default_directory_row.set_subtitle(value);
+			enable_action('file.set', 'download-directory');
 		}
+	};
 
-		const { output: _files, output_signals: file_signals, enable_action, disable_action } = useFile(window, builder, window);
+	on_download_directory_changed();
 
-		const default_directory_row = /** @type {Adw.ActionRow | null} */ (builder.get_object('default_directory_row'));
-		if (!default_directory_row) throw new Error;
+	settings.connect('changed::download-directory', on_download_directory_changed);
 
-		const on_download_directory_changed = () => {
-			const value = settings.get_string('download-directory');
-			if (value === '') {
-				default_directory_row.set_subtitle(_('Select Location'));
-				disable_action('file.set', 'download-directory');
-			} else {
-				default_directory_row.set_subtitle(value);
-				enable_action('file.set', 'download-directory');
-			}
-		};
-
-		on_download_directory_changed();
-
-		settings.connect('changed::download-directory', on_download_directory_changed);
-
-		file_signals.connect('changed::download-directory', (_obj, key, file) => {
-			const value = file.get_path() || '';
-			settings.set_string(key, retract_path(value));
-		});
-
-		const parent_window = application.get_active_window();
-		if (parent_window) {
-			window.set_transient_for(parent_window);
-		}
-
-		window.present();
+	file_signals.connect('changed::download-directory', (_obj, key, file) => {
+		const value = file.get_path() || '';
+		settings.set_string(key, retract_path(value));
 	});
-	application.add_action(show_pref);
-	application.set_accels_for_action('app.show-preferences', ['<Primary>comma']);
 
-	const show_about = new Gio.SimpleAction({
-		name: 'show-about',
-	});
-	show_about.connect('activate', () => {
-		const window = AboutWindow.new_from_appdata(`/com/github/kinten108101/Boki/com.github.kinten108101.Boki.metainfo.xml`, 'beta');
+	const parent_window = application.get_active_window();
+	if (parent_window) {
+		window.set_transient_for(parent_window);
+	}
 
-		window.set_debug_info(`flatpak=${globalThis.is_built_for_flatpak}\n`);
+	window.present();
+});
+application.add_action(show_pref);
+application.set_accels_for_action('app.show-preferences', ['<Primary>comma']);
 
-		const parent_window = application.get_active_window();
-		if (parent_window) {
-			window.set_transient_for(parent_window);
-		}
+const show_about = new Gio.SimpleAction({
+	name: 'show-about',
+});
+show_about.connect('activate', () => {
+	const window = AboutWindow.new_from_appdata(`/com/github/kinten108101/Boki/com.github.kinten108101.Boki.metainfo.xml`, 'beta');
 
-		window.present();
-	});
-	application.add_action(show_about);
+	window.set_debug_info(`flatpak=${globalThis.is_built_for_flatpak}\n`);
 
-	application.set_accels_for_action('window.close', ['<Primary>w']);
+	const parent_window = application.get_active_window();
+	if (parent_window) {
+		window.set_transient_for(parent_window);
+	}
 
+	window.present();
+});
+application.add_action(show_about);
+application.set_accels_for_action('window.close', ['<Primary>w']);
+
+if (globalThis.is_devel) {
 	const test_click_widget = new Gio.SimpleAction({
 		name: 'test.click-widget',
 		parameter_type: GLib.VariantType.new('s'),
@@ -153,11 +150,9 @@ export function Application() {
 		obj.activate();
 	});
 	application.add_action(test_click_widget);
-
-  	application.connect('activate', () => {
-    	const mainWindow = Window(application, settings);
-    	mainWindow.present();
-  	});
-  
-	return application;
 }
+
+application.connect('activate', () => {
+	const mainWindow = Window(application, settings);
+	mainWindow.present();
+});
